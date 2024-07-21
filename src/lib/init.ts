afbc8604c3c0d9e2f319af155d0c421fa9e89fb6
narlen <narlenhua@qq.com>
2024-07-22 01:10:57 +0800

    增加了自动修复重启的功能，适配了火狐浏览器，可以设置。

--- src/lib/init.ts
+++ src/lib/init.ts
@@ -91,21 +91,18 @@ async function initMainWindow() {
   let menuItem2 = await ingector.creatIngectorWindow();
   // 工作区
   let workSpace: HTMLElement = windowElementer.createItem('div', `fabricMainWindow-workspace`, fabricStyle.class["fabric-window-workspace"]);
-  let button0 = windowElementer.createItem('button', undefined, undefined, '打开PageSpy');
-  button0.style.backgroundColor = 'yellow';
+
+  let button0 = windowElementer.createItem('button', undefined, undefined, '打开Eruda');
+  button0.style.backgroundColor = 'green';
   // @ts-ignore
-  button0.onclick = () => { window.$harbor = new DataHarborPlugin(); window.$rrweb = new RRWebPlugin();[window.$harbor, window.$rrweb].forEach(p => { PageSpy.registerPlugin(p) }), window.$pageSpy = new PageSpy() };
-  let button1 = windowElementer.createItem('button', undefined, undefined, '打开Eruda');
-  button1.style.backgroundColor = 'green';
+  button0.onclick = () => { eruda.init(); eruda.position({ x: window.innerWidth - 100, y: window.innerHeight - 50 }); };
+  let button1 = windowElementer.createItem('button', undefined, undefined, '关闭Eruda');
+  button1.style.backgroundColor = 'yellow';
   // @ts-ignore
-  button1.onclick = () => { eruda.init(); eruda.position({ x: window.innerWidth - 100, y: window.innerHeight - 50 }); };
-  let button2 = windowElementer.createItem('button', undefined, undefined, '关闭Eruda');
-  button2.style.backgroundColor = 'yellow';
-  // @ts-ignore
-  button2.onclick = () => { eruda.destroy(); }
-  let button3 = windowElementer.createItem('button', undefined, undefined, '设置是否注入Eruda');
-  button3.style.backgroundColor = 'green';
-  button3.onclick = () => {
+  button1.onclick = () => { eruda.destroy(); }
+  let button2 = windowElementer.createItem('button', undefined, undefined, '设置是否自动启动Eruda');
+  button2.style.backgroundColor = 'green';
+  button2.onclick = () => {
     let allowTemp = localStorage.getItem('allowEruda');
     if (allowTemp == true.toString())
       localStorage.setItem('allowEruda', false.toString());
@@ -114,41 +111,87 @@ async function initMainWindow() {
     // @ts-ignore
     _alert(`allowEruda设置为${localStorage.getItem('allowEruda')}`);
   }
-  let button4 = windowElementer.createItem('button', undefined, undefined, '设置是否注入PageSpy');
+  let button3 = windowElementer.createItem('button', undefined, undefined, '设置遇到错误是否自动重启');
+  button3.style.backgroundColor = 'yellow';
+  // @ts-ignore
+  button3.onclick = () => {
+    let allowTemp = localStorage.getItem('allowAutoReload');
+    if (allowTemp == false.toString())
+      localStorage.setItem('allowAutoReload', true.toString());
+    else
+      localStorage.setItem('allowAutoReload', false.toString());
+    // @ts-ignore
+    _alert(`allowAutoReload设置为${localStorage.getItem('allowAutoReload')}`);
+  }
+  let button4 = windowElementer.createItem('button', undefined, undefined, '打开PageSpy');
   button4.style.backgroundColor = 'yellow';
   // @ts-ignore
-  button4.onclick = () => {
-    let allowTemp = localStorage.getItem('allowPageSpy');
-    if (allowTemp == true.toString())
-      localStorage.setItem('allowPageSpy', false.toString());
-    else
-      localStorage.setItem('allowPageSpy', true.toString());
+  button4.onclick = async () => {
+    async function sleep(ms: number) {
+      return new Promise((resolve) => setTimeout(resolve, ms));
+    }
+
+    for (let i = 0; i < 10; i++) {
+      // @ts-ignore
+      if (window.DataHarborPlugin != undefined && window.RRWebPlugin != undefined && window.PageSpy != undefined) {
+        // @ts-ignore
+        window.$harbor = new DataHarborPlugin(); window.$rrweb = new RRWebPlugin();[window.$harbor, window.$rrweb].forEach(p => { PageSpy.registerPlugin(p) }), window.$pageSpy = new PageSpy()
+        break;
+      }
+      await sleep(100);
+    }
     // @ts-ignore
-    _alert(`allowPageSpy设置为${localStorage.getItem('allowPageSpy')}`);
+    if (window.DataHarborPlugin != undefined && window.RRWebPlugin != undefined && window.PageSpy != undefined) {
+      console.log('PageSpy初始化成功');
+      // @ts-ignore
+      _alert('PageSpy初始化成功');
+    }
+    else {
+      console.error('PageSpy初始化失败');
+      // @ts-ignore
+      _alert('PageSpy初始化失败');
+    }
+
   }
-  let button5 = windowElementer.createItem('button', undefined, undefined, '设置PageSpy服务器地址');
-  button5.style.backgroundColor = 'green';
+  let button5 = windowElementer.createItem('button', undefined, undefined, '注入PageSpy');
+  button5.style.backgroundColor = 'yellow';
   // @ts-ignore
-  button5.onclick = () => {
+  button5.onclick = async () => {
+    let pageSpyURL = localStorage.getItem('pageSpyURL');
+    if (pageSpyURL == null || pageSpyURL == '') {
+      localStorage.setItem('pageSpyURL', '');
+      // @ts-ignore
+      _alert('请先设置PageSpy调试服务器地址');
+      console.error('请先设置PageSpy调试服务器地址');
+      return;
+    } else {
+      let ps1 = document.createElement('script');
+      ps1.setAttribute('crossorigin', 'anonymous');
+      ps1.src = `${pageSpyURL}/page-spy/index.min.js`;
+      let ps2 = document.createElement('script');
+      ps2.setAttribute('crossorigin', 'anonymous');
+      ps2.src = `${pageSpyURL}/plugin/data-harbor/index.min.js`;
+      let ps3 = document.createElement('script');
+      ps3.setAttribute('crossorigin', 'anonymous');
+      ps3.src = `${pageSpyURL}/plugin/rrweb/index.min.js`;
+      document.body.append(ps1, ps2, ps3);
+      // @ts-ignore
+      _alert('PageSpy已注入');
+    }
+  }
+  let button6 = windowElementer.createItem('button', undefined, undefined, '设置PageSpy服务器地址');
+  button6.style.backgroundColor = 'green';
+  // @ts-ignore
+  button6.onclick = () => {
     let urlTemp = prompt("不要轻易输入陌生人给的调试服务器地址，远程调试人员将看到你的所有数据！！！\n请输入PageSpy服务器地址:");
     if (urlTemp != null && urlTemp != "") {
       localStorage.setItem('pageSpyURL', urlTemp);
     }
   }
-  let button6 = windowElementer.createItem('button', undefined, undefined, '设置遇到错误是否自动重启');
-  button6.style.backgroundColor = 'yellow';
-  // @ts-ignore
-  button6.onclick = () => {
-    let allowTemp = localStorage.getItem('allowAutoReload');
-    if (allowTemp == true.toString())
-      localStorage.setItem('allowAutoReload', false.toString());
-    else
-      localStorage.setItem('allowAutoReload', true.toString());
-    // @ts-ignore
-    _alert(`allowAutoReload设置为${localStorage.getItem('allowAutoReload')}`);
-  }
-  let dis = windowElementer.createItem('p', undefined, undefined, '<a href="https://www.pagespy.org/">PageSpy远程调试工具官网</a>  具备调试修复功能，当因为网络等原因无法进入时，可以点击在右下角打开调试工具，运行"iirosesave()"将保存存档，运行"iiroserepair()"将尝试修复程序，修复后重载可能可以进入,运行"closeconsole()"将关闭调试工具。');
-  workSpace.append(button0, button1, button2, button3, button4, button5, dis);
+
+  let dis1 = windowElementer.createItem('p', undefined, undefined, '实验性：');
+  let dis2 = windowElementer.createItem('p', undefined, undefined, '<a href="https://www.pagespy.org/">PageSpy远程调试工具官网</a>  具备调试修复功能，当因为网络等原因无法进入时，可以点击在右下角打开调试工具，运行"iirosesave()"将保存存档，运行"iiroserepair()"将尝试修复程序，修复后重载可能可以进入,运行"closeconsole()"将关闭调试工具。');
+  workSpace.append(button0, button1, button2, button3, dis1, button4, button5, button6, dis2);
   let fabiricMianWindow = windowElementer.createFabrcWindow('fabricMainWindow', 400, workSpace);
   // 关闭窗口
   windowElementer.closeElement(fabiricMianWindow);
